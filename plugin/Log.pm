3294062c74e1a26063919094536107994cbd25ee
Nicola Worthington <nicolaw@tfb.net>
2006-02-01 18:22:48 +0000

    Adding
    git-svn-id: file:///var/tmp/data/svnroot/botbot2@35 610ec7d1-1c12-4784-ad90-13d12571cf29

---
+++ plugin/Log.pm
@@ -0,0 +1,16 @@
+package plugin::Log;
+use base plugin;
+use strict;
+
+sub handle {
+	my ($self,$event,$responded) = @_;
+	return if $event->{alarm};
+
+	$self->log($event->{raw});
+
+	# Didn't respond to the user
+	return 0;
+}
+
+1;
+
