877b252ce08aad2f0c79081c3d13a0a028e2a73d
Nicola Worthington <nicolaw@tfb.net>
2006-02-01 16:33:29 +0000

    git-svn-id: file:///var/tmp/data/svnroot/botbot2@25 610ec7d1-1c12-4784-ad90-13d12571cf29

--- plugin/HelloWorld.pm
+++ plugin/HelloWorld.pm
@@ -1,15 +1,15 @@
-package plugin::helloworld;
+package plugin::HelloWorld;
 use base plugin;
 use strict;
-use Data::Dumper;
 
 sub handle {
 	my ($self,$event,$responded) = @_;
-
-	warn Dumper($event);
-	warn Dumper($responded);
-	warn Dumper($self);
-
+	return if $event->{alarm};
+	$self->{talker}->whisper(
+			$event->{list} ? $event->{list} : $event->{person},
+			"Hello $event->{person}",
+		);
+	return "I spoke";
 }
 
 1;
