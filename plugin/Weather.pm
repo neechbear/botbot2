ba024a357aa70c571bd78b236f19b65ecabc70c0
Nicola Worthington <nicolaw@tfb.net>
2006-02-05 19:49:45 +0000

    git-svn-id: file:///var/tmp/data/svnroot/botbot2@101 610ec7d1-1c12-4784-ad90-13d12571cf29

--- plugin/Weather.pm
+++ plugin/Weather.pm
@@ -1,30 +1,85 @@
-package plugin::Pook;
+package plugin::Weather;
 use base plugin;
 use strict;
+use Weather::Cached;
 
-our $DESCRIPTION = 'Pooks like a fish with other people do';
+our $DESCRIPTION = 'Return weather information from weather.com';
 
 sub handle {
 	my ($self,$event,$responded) = @_;
 
 	return if $event->{alarm};
-	return undef unless int(rand(3)) == 2;
-	return unless $event->{command} =~ /^(poo+ks?\s*)+\b/i ||
-					$event->{command} =~ /^(poo+ks?\s*)+$/i;
+	return unless $event->{msgtype} =~ /^OBSERVE TALK|TALK|LISTTALK|TELL$/;
+	return unless $event->{command} =~ /^weather|forecast$/i;
 
-	if (int(rand(3)) == 2) {
-		$self->{talker}->say(
-				($event->{list} !~ /\@/ ? "<<$event->{list}" : '<@Public')." ".
-				"pooks"
-			);
+	my $cachedir = './data/weathercache/';
+	mkdir $cachedir unless -e $cachedir;
+	my %params = (
+			'cache'      => $cachedir,
+			'current'    => $event->{command} =~ /^weather$/i ? 1 : 0,
+			'forecast'   => $event->{command} =~ /^fore?cast$/i ? 2 : 0,
+			'links'      => 1,
+			'units'      => 's',
+			'timeout'    => 5,
+			'debug'      => 0,
+			'partner_id' => 'somepartnerid',
+			'license'    => '12345678',
+		);
+
+	my $cached_weather = Weather::Cached->new(%params);
+	my $arg = join(' ',@{$event->{cmdargs}});
+
+	if (my $locations = $cached_weather->search($arg)) {
+
+		# I know, ... i might do something other than just return one day
+		foreach (keys %{$locations}) {
+			my $weather = $cached_weather->get_weather($_);
+			my @reply = ();
+
+			# Forecast
+			if ($event->{command} =~ /^fore?cast$/i) {
+				my $tomorrow = $weather->{dayf}->{day}->[1];
+				my $str = sprintf('%s, %s: %s, hi %s %s / low %s %s',
+						$tomorrow->{dt},
+						$weather->{loc}->{dnam}, $tomorrow->{part}->[0]->{t},
+						$tomorrow->{hi}, $weather->{head}->{ut},
+						$tomorrow->{low}, $weather->{head}->{ut},
+					);
+				$str .= sprintf('. %s %s %s wind',
+						$tomorrow->{part}->[0]->{wind}->{s}, $weather->{head}->{us},
+						$tomorrow->{part}->[0]->{wind}->{t}, 
+					);
+				push @reply, $str;
+
+			# Current conditions
+			} else {
+				my $str = sprintf('%s: %s, temp %s %s',
+						$weather->{loc}->{dnam}, $weather->{cc}->{t},
+						$weather->{cc}->{tmp}, $weather->{head}->{ut},
+					);
+				$str .= sprintf(', (feels like %s %s)',
+						$weather->{cc}->{flik}, $weather->{head}->{ut},
+					) if $weather->{cc}->{flik} != $weather->{cc}->{tmp};
+				$str .= sprintf('. %s %s %s wind',
+						$weather->{cc}->{wind}->{s}, $weather->{head}->{us},
+						$weather->{cc}->{wind}->{t}, 
+					);
+				push @reply, $str;
+			}
+
+			$self->{talker}->whisper(
+					($event->{list} ? $event->{list} : $event->{person}),
+					$_
+				) for @reply;
+			return "Returned weather information for @{$event->{cmdargs}}";
+		}
+
 	} else {
-		$self->{talker}->say(
-				($event->{list} !~ /\@/ ? ">>$event->{list}" : '>@Public')." ".
-				"pook"
+		$self->{talker}->whisper($event->{person},
+				"I don't know where $arg is; try another location?"
 			);
-	}
-
-	return "pooked like a fish";
+		return 0;
+	} 
 }
 
 1;
