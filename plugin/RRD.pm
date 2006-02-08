4e08314c5fb8532915d0ca48b9ee53d0e93f29ec
Nicola Worthington <nicolaw@tfb.net>
2006-02-08 17:26:42 +0000

    git-svn-id: file:///var/tmp/data/svnroot/botbot2@152 610ec7d1-1c12-4784-ad90-13d12571cf29

--- plugin/RRD.pm
+++ plugin/RRD.pm
@@ -9,9 +9,72 @@ our $DESCRIPTION = 'Log talker usage statistics to an RRD file';
 sub handle {
 	my ($self,$event,$responded) = @_;
 
-	return if $event->{alarm};
-	return unless $event->{command} =~ /^(dict(ionary)?|define)$/i;
-	return unless $event->{msgtype} =~ /^OBSERVE TALK|TALK|TELL|LISTTALK$/;
+	$self->{chunk} = (time()/60)*60;
+	$self->{lastchunk} ||= $self->{chunk};
+
+	$self->{rrdfile} ||= './activity.rrd';
+	$self->{rrd} ||= new RRD::Simple;
+
+	$self->{counter} ||= {GROUP => 0, PRIVATE => 0, SHOUT => 0, LIST => 0};
+	$self->{alarmcounter} ||= 0;
+
+	$self->{alarmcounter}++ if $event->{alarm};
+	if ($event->{alarm} && $self->{alarmcounter} > 30) {
+		$self->{rrd}->graph($self->{rrdfile},
+				destination => './data/',
+				title => 'Talker Activity',
+				width => 560,
+				vertical_label => 'Messages per minute',
+				slope_mode => undef,
+				units_exponent => 0,
+				interlaced => undef,
+				line_thickness => 1,
+				source_labels => {
+						GROUP => '@Public',
+						SHOUT => '!shouts',
+						LIST => '%lists',
+					},
+				source_colors => {
+						GROUP => '0000cc',
+						SHOUT => 'cc0000',
+						LIST => '00cc00',
+					},
+				color => [ (
+						'BACK#d5e5FF',
+						'SHADEA#C8C8FF',
+						'SHADEB#9696BE',
+						'ARROW#61B51B',
+						'GRID#404852',
+						'MGRID#67C6DE',
+					) ],
+			) if -f $self->{rrdfile};
+		$self->{alarmcounter} = 0;
+	}
+
+	return unless $event->{msgtype} =~ /^(OBSERVE |TALK|TELL|SHOUT|LIST)/;
+
+	$self->{counter}->{GROUP}++   if $event->{msgtype} =~ /^(OBSERVE |TALK)/;
+	$self->{counter}->{PRIVATE}++ if $event->{msgtype} eq 'TELL';
+	$self->{counter}->{LIST}++    if $event->{msgtype} =~ /^LIST/;
+	$self->{counter}->{SHOUT}++   if $event->{msgtype} eq 'SHOUT';
+
+	unless (-f $self->{rrdfile}) {
+		$self->{rrd}->create($self->{rrdfile},
+				GROUP   => 'GAUGE',
+				PRIVATE => 'GAUGE',
+				LIST    => 'GAUGE',
+				SHOUT   => 'GAUGE',
+			);
+	}
+
+	if ($self->{lastchunk} ne $self->{chunk}) {
+		$self->{rrd}->update($self->{rrdfile},$self->{chunk},
+				map { $_ => $self->{counter}->{$_} } keys %{$self->{counter}}
+			);
+		$self->{counter} = {GROUP => 0, PRIVATE => 0, SHOUT => 0, LIST => 0};
+	}
+
+	$self->{lastchunk} = $self->{chunk};
 
 	return 0;
 }
@@ -19,95 +82,4 @@ sub handle {
 1;
 
 __END__
-
-my $rrd = '/home/system/colloquy/botbot/logs/botbot.rrd';
-unless (-f $rrd) {
-	RRD::Simple->create($rrd,
-			OBSERVED => 'GAUGE',
-			LIST => 'GAUGE',
-			SHOUT => 'GAUGE'
-		);
-}
-
-my $hits = {OBSERVED => {}, SHOUT => {}, LIST => {}};
-my ($first,$last) = (0,0);
-my $lastUpdate = RRD::Simple->last($rrd);
-my %months = (qw(Jan 0 Feb 1 Mar 2 Arp 3 May 4 Jun 5
-			Jul 6 Aug 7 Sep 8 Oct 9 Nov 10 Dec 11));
-
-$|++;
-while (<>) {
-	if (my ($wday,$mon,$mday,$hour,$min,$sec,$year,$type) = $_
-			=~ /^\[(...) (...) (..) (..):(..):(..) (....)\] \[(\S+)/) {
-		$year -= 1900;
-		my @val = split(/\s/,sprintf('%d %d %d %d %d %d',
-				$sec,$min,$hour,$mday,$months{$mon},$year));
-
-		my $time = timelocal(@val);
-		next if $lastUpdate >= $time;
-		$first ||= $time; $last = $time;
-
-		if ($type =~ /^OBSERVED|LIST|SHOUT/) {
-			print ".";
-			$type = 'LIST' if $type =~ /^LIST/;
-			my $period = ($time / 300) * 300;
-			$hits->{$type}->{$period}++;
-		}
-	}
-}
-print "\n";
-
-die "No new data" unless $first > $lastUpdate;
-die "Wasn't anything new in the log files" unless $first && $last;
-$first = ($first / 300) * 300;
-$last = ($last / 300) * 300;
-
-#print join(', ',RRD::Simple->sources($rrd))."\n";
-#print RRD::Simple->last($rrd)."\n";
-
-for (my $time = $first; $time <= $last; $time += 300) {
-	my @vals;
-	for my $type (keys %{$hits}) {
-		push @vals, ($type,(exists $hits->{$type}->{$time} ?
-						$hits->{$type}->{$time} : 0));
-	}
-	#print "RRD::Simple->update('$rrd',$time,'".join("','",@vals)."');\n";
-	eval{RRD::Simple->update($rrd,$time,@vals);};
-}
-
-RRD::Simple->graph($rrd,
-		destination => '/home/system/apache/htdocs/talker',
-		title => 'Talker Activity',
-		width => 560,
-		vertical_label => 'Messages/5min',
-		slope_mode => undef,
-		units_exponent => 0,
-		interlaced => undef,
-		line_thickness => 1,
-		source_labels => {
-				OBSERVED => '@Public',
-				SHOUT => '!shouts',
-				LIST => '%lists',
-			},
-		source_colors => {
-				OBSERVED => '0000cc',
-				SHOUT => 'cc0000',
-				LIST => '00cc00',
-			},
-		color => [ (
-				'BACK#d5e5FF',
-				'SHADEA#C8C8FF',
-				'SHADEB#9696BE',
-				'ARROW#61B51B',
-				'GRID#404852',
-				'MGRID#67C6DE',
-			) ],
-	);
-
-
-#!/bin/bash
-
-umask 0002
-cd /home/system/colloquy/botbot && \
-	(zcat logs/botbot.log.1.gz;cat logs/botbot.log) | ./rrd.pl
 
