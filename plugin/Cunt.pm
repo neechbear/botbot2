f9a4a27a8e4f8ee5b6da2f9ff6c49ee9e26b154d
Nicola Worthington <nicolaw@tfb.net>
2006-03-09 22:07:31 +0000

    git-svn-id: file:///var/tmp/data/svnroot/botbot2@300 610ec7d1-1c12-4784-ad90-13d12571cf29

--- plugin/Cunt.pm
+++ plugin/Cunt.pm
@@ -13,10 +13,31 @@ sub handle {
 	my @words = qw(beaver clit cock cooter cornhole cunt
 				douche fuck jizz poontang prick pussy
 				queef snatch tits twat fuckhole);
+	my %responses = ();
+
+	my $file = "data/rude.txt";
+	if (-f $file && open(RUDE,"<","data/rude.txt")) {
+		@words = ();
+		while (local $_ = <RUDE>) {
+			s/^\s+|\s+$//g;
+			my ($word,$response) = $_ =~ /^(\S+)(?:\s+(.+))?/;
+			push @words,$word;
+			$responses{$word} = $response;
+		}
+		close(RUDE);
+	}
 
 	return unless grep(/^$event->{command}$/i,@words);
 
-	my $data = `figlet $event->{command}`;
+	my $data = '';
+	if (!defined($responses{$event->{command}}) &&
+			$event->{command} =~ /^[a-z0-9]+$/i) {
+		$data = `figlet "$event->{command}"`;
+
+	} elsif ($responses{$event->{command}} =~ /^[ a-zA-Z0-9,\.\-\+\=\!]+$/i) {
+		$data = `figlet "$responses{$event->{command}}"`;
+	}
+
 	$self->{talker}->whisper(
 			($event->{list} ? $event->{list} : $event->{person}),
 			" $_"
