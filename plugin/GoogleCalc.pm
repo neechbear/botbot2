9e7f6bc0b0df737c69f219239d22d55c87eb6a01
Nicola Worthington <nicolaw@tfb.net>
2006-05-15 20:25:31 +0000

    git-svn-id: file:///var/tmp/data/svnroot/botbot2@444 610ec7d1-1c12-4784-ad90-13d12571cf29

---
+++ plugin/GoogleCalc.pm
@@ -0,0 +1,41 @@
+package plugin::GoogleCalc;
+use base plugin;
+use strict;
+use WWW::Google::Calculator;
+
+our $DESCRIPTION = 'Google Calculator';
+our %CMDHELP = (
+		'calc <expression> ' => 'Return results of <expression> using Google\'s calculator'
+	);
+
+sub handle {
+	my ($self,$event,$responded) = @_;
+
+	return if $event->{alarm};
+	return unless $event->{command} =~ /^(calc|expr|eval)$/i;
+	return unless $event->{msgtype} =~ /^OBSERVE TALK|TALK|LISTTALK|TELL$/;
+
+	return 0 unless length($event->{cmdargs}->[0]);
+	my $query = join(' ',@{$event->{cmdargs}});
+
+	my $calc = WWW::Google::Calculator->new;
+	my $reply = $calc->calc($query);
+
+	if (defined $reply && $reply) {
+		$self->{talker}->whisper(
+				($event->{list} ? $event->{list} : $event->{person}),
+				$reply
+			);
+		return "Returned result of $query";
+
+	} else {
+		$self->{talker}->whisper(
+				$event->{person},
+				'I don\'t know'
+			);
+		return 0;
+	}
+}
+
+1;
+
