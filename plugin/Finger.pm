e68f6e9cba7c71ea8ed00b05ec3c7ae6f9218eba
Nicola Worthington <nicolaw@tfb.net>
2006-05-19 16:34:59 +0000

    git-svn-id: file:///var/tmp/data/svnroot/botbot2@453 610ec7d1-1c12-4784-ad90-13d12571cf29

---
+++ plugin/Finger.pm
@@ -0,0 +1,41 @@
+package plugin::Finger;
+use base plugin;
+use strict;
+use Net::Finger;
+
+our $DESCRIPTION = 'Finger people';
+our %CMDHELP = (
+		'figer <user@host>' => 'Return finger results for <user@host>'
+	);
+
+sub handle {
+	my ($self,$event,$responded) = @_;
+
+	return if $event->{alarm};
+	return unless $event->{command} =~ /^finger$/i;
+	return unless $event->{msgtype} =~ /^OBSERVE TALK|TALK|LISTTALK|TELL$/;
+
+	return 0 unless length($event->{cmdargs}->[0]);
+
+	my @response = ();
+	eval {
+		@response = finger($event->{cmdargs}->[0]);
+	};
+
+	if (@response) {
+		$self->{talker}->whisper(
+				($event->{list} ? $event->{list} : $event->{person}),
+				$_
+			) for @response;
+		return "Returned finger results for $event->{cmdargs}->[0]";
+	}
+
+	$self->{talker}->whisper(
+			$event->{person},
+			"Don't know who they are $@"
+		);
+	return 0;
+}
+
+1;
+
