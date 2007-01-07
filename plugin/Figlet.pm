bbbc97434d74be44bf03ddbc23da40f773fdad7b
Nicola Worthington <nicolaw@tfb.net>
2007-01-07 22:18:43 +0000

    git-svn-id: file:///var/tmp/data/svnroot/botbot2@905 610ec7d1-1c12-4784-ad90-13d12571cf29

--- plugin/Figlet.pm
+++ plugin/Figlet.pm
@@ -1,5 +1,6 @@
 package plugin::Figlet;
 use base plugin;
+use String::ShellQuote qw();
 use strict;
 
 our $DESCRIPTION = 'Figlets words';
@@ -17,12 +18,20 @@ sub handle {
 
 	if ($event->{text} =~ /^$self->{config}->{username}\s*,?\s+(s+h+|q+u+i+e+t+|s+h+u+[td]+\s*u+p+|s+h+u+s+h+|stfu)/i ||
 		$event->{text} =~ /^\s*(s+h+|q+u+i+e+t+|s+h+u+[td]+\s*u+p+|s+h+u+s+h+|stfu)\s*,?\s*$self->{config}->{username}/i) {
-		$self->{quiet} = 60;
+		$self->{quiet} = 300;
 		return 0;
 	}
 
+	my $nick_margin = 13;
+	my $default_width = 79;
+	my $width_limit = $default_width - $nick_margin - 1;
+	if ($event->{list} =~ /^%/) {
+		$width_limit -= length($event->{list})-1 + 3;
+	}
+
 	my $fontdir = '/usr/share/figlet/';
-	my $cmd = "/usr/bin/figlet -w 65 ";
+	my $cmd = "/usr/bin/figlet -w $width_limit ";
+
 	if ($event->{cmdargs}->[0] eq '-f' && -f "$fontdir/$event->{cmdargs}->[1].flf") {
 		$cmd .= " -f $event->{cmdargs}->[1].flf ";
 		shift @{$event->{cmdargs}};
@@ -31,9 +40,26 @@ sub handle {
 
 	my $output = '';
 	my $input = join(' ',@{$event->{cmdargs}});
-	if ($input =~ /^[ a-zA-Z0-9,\<\>\(\)\.\-\+\=\!\?\']+$/i) {
-		$output = `$cmd "$input"`;
+	if (length($input) > 40) {
+		my @responses = (
+				"That's excessively long to figlet don't you think? Behave!",
+				"That's excessively long to figlet don't you think?",
+				"You're kidding right? That much for a figlet?",
+				"Shhh, behave.",
+				"That's rather long isn't it?",
+				"Trim it done babes!",
+			);
+		$self->{talker}->whisper(
+				($event->{list} ? $event->{list} : $event->{person}),
+				$responses[rand(@responses)],
+			) for split(/\n/,$output);
+		return 0;
 	}
+
+	$input = String::ShellQuote::shell_quote($input);
+#	if ($input =~ /^[ a-zA-Z0-9,\<\>\(\)\.\-\+\=\!\?\']+$/i) {
+		$output = `$cmd $input`;
+#	}
 
 	return 0 unless $output;
 
